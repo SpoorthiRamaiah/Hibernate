d4405ad95528fc11f02e60ddf442fded5f8b9005
SpoorthiRamaiah <spoorthiramaiah@gmail.com>
2023-06-03 12:01:25 +0530

    Person

---
+++ Person.java
@@ -0,0 +1,47 @@
+package com.jsp;
+
+import org.hibernate.annotations.Entity;
+
+@Entity
+public class Person {
+	private int id;
+	private String name;
+	private String address;
+	private String phno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	public Person(int id, String name, String address, String phno) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.phno = phno;
+	}
+	
+	}
+	
+
+
+
